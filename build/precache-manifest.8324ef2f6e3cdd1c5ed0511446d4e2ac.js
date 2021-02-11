59ffbe04afccf8f5fb93bf16da0a56c53427eb4d
byronmosley <mosleyb91@outlook.com>
2021-02-11 14:38:01 -0700

    added npm build folder

---
+++ build/precache-manifest.8324ef2f6e3cdd1c5ed0511446d4e2ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f669e130cb495e78b439f3bb3c227393",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbcc3053d2b29bc102b8",
+    "url": "/static/js/2.1348bdf7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1348bdf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b01142c0ae96ce7c31",
+    "url": "/static/js/main.5992d352.chunk.js"
+  },
+  {
+    "revision": "08d876ca5bd05d3ea4f0",
+    "url": "/static/js/runtime-main.b2a68d07.js"
+  }
+]);
